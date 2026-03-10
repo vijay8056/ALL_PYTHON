812ffc21c0d0cac52daf7aba7646a021fc66a3de
lrani1 <lrani@matson.com>
2026-03-10 00:10:48 -0700

    initial commit9

--- age_calc.py
+++ age_calc.py
@@ -1,6 +1,24 @@
-import datetime
+from datetime import datetime
 
-print("Enter your birthdate (YYYY-MM-DD): ")
+print("Enter Number: ")
 
-x= input()
+x= int(input())
 print("value enetered:", x)
+y=0
+
+while x>y:
+    print (y)
+    y=y+1
+
+def calc_age(date_of_birth):
+    birthdate = datetime.strptime(date_of_birth, "%Y-%m-%d")
+    today = datetime.today()
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    print("Your age is:", age)
+
+
+if '__main__' == __name__:
+    print("Enter your birthdate (YYYY-MM-DD): ")
+    date_of_birth = input()
+    calc_age(date_of_birth);
+
